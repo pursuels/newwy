16ca454a07a6a8f70861019d9b70ce87762fb15c
pursuels <wangzipengyx@163.com>
2014-10-16 12:38:54 +0800

    a

---
+++ files/menu.js
@@ -0,0 +1,124 @@
+function mmLoadMenus() {
+  if (window.mm_menu_1031135809_0) return;
+  window.mm_menu_1031135809_0 = new Menu("root",65,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,true);
+  mm_menu_1031135809_0.addMenuItem("西财新闻网","http://ghy.swufe.edu.cn/swufenews/");
+  mm_menu_1031135809_0.addMenuItem("西南财大报","http://swufe.cuepa.cn/");
+  mm_menu_1031135809_0.addMenuItem("校园电视","http://etv.swufe.edu.cn");
+  mm_menu_1031135809_0.addMenuItem("媒体西财","http://www.swufe.edu.cn/1459.html");
+   mm_menu_1031135809_0.hideOnMouseOut=true;
+   mm_menu_1031135809_0.bgColor='#003a66';
+   mm_menu_1031135809_0.menuBorder=0;
+   mm_menu_1031135809_0.menuLiteBgColor='';
+   mm_menu_1031135809_0.menuBorderBgColor='';
+   //
+window.mm_menu_1031140637_0 = new Menu("root",65,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,true);
+  mm_menu_1031140637_0.addMenuItem("学校简介","http://www.swufe.edu.cn/1679.html");
+  mm_menu_1031140637_0.addMenuItem("学校领导","http://www.swufe.edu.cn/3070.html");
+  mm_menu_1031140637_0.addMenuItem("历史沿革","http://www.swufe.edu.cn/1680.html");
+  mm_menu_1031140637_0.addMenuItem("校园风光","http://www.swufe.edu.cn/1684.html");
+  mm_menu_1031140637_0.addMenuItem("校园导航","http://www.swufe.edu.cn/2245.html");
+   mm_menu_1031140637_0.hideOnMouseOut=true;
+   mm_menu_1031140637_0.bgColor='#003a66';
+   mm_menu_1031140637_0.menuBorder=0;
+   mm_menu_1031140637_0.menuLiteBgColor='';
+   mm_menu_1031140637_0.menuBorderBgColor='';
+
+window.mm_menu_1031140638_0 = new Menu("root",65,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,true);
+  mm_menu_1031140638_0.addMenuItem("师资概况","http://www.swufe.edu.cn/2175.html");
+  mm_menu_1031140638_0.addMenuItem("教授风采","http://www.swufe.edu.cn/2178.html");
+  //mm_menu_1031140638_0.addMenuItem("教授风采","szjsjs.html");
+  mm_menu_1031140638_0.addMenuItem("海外学者","http://www.swufe.edu.cn/1679.html");
+  mm_menu_1031140638_0.addMenuItem("人才招聘","http://rcpt.swufe.edu.cn");
+   mm_menu_1031140638_0.hideOnMouseOut=true;
+   mm_menu_1031140638_0.bgColor='#003a66';
+   mm_menu_1031140638_0.menuBorder=0;
+   mm_menu_1031140638_0.menuLiteBgColor='';
+   mm_menu_1031140638_0.menuBorderBgColor='';
+
+ window.mm_menu_1031140711_0 = new Menu("root",65,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,false);
+  mm_menu_1031140711_0.addMenuItem("本科生教育","http://jwc.swufe.edu.cn");
+  mm_menu_1031140711_0.addMenuItem("研究生教育","http://graduate.swufe.edu.cn");
+mm_menu_1031140711_0.addMenuItem("专业学位教育","http://www.swufe.edu.cn/2183.html");
+  mm_menu_1031140711_0.addMenuItem("继续教育","http://xczx.swufe.edu.cn/");
+  mm_menu_1031140711_0.addMenuItem("留学生","http://international.swufe.edu.cn");
+mm_menu_1031140711_0.addMenuItem("出国预备教育","http://liuxue.swufe.edu.cn");
+  mm_menu_1031140711_0.addMenuItem("英语自主学习网站","http://english.swufe.edu.cn/");
+  mm_menu_1031140711_0.addMenuItem("财经数学网","http://femath.swufe.edu.cn/");
+   mm_menu_1031140711_0.hideOnMouseOut=true;
+   mm_menu_1031140711_0.bgColor='#003a66';
+   mm_menu_1031140711_0.menuBorder=0;
+   mm_menu_1031140711_0.menuLiteBgColor='';
+   mm_menu_1031140711_0.menuBorderBgColor='';
+
+window.mm_menu_1031140744_0 = new Menu("root",65,28,"科学研究",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,true);
+  mm_menu_1031140744_0.addMenuItem("科研处","http://kyc.swufe.edu.cn");
+  //mm_menu_1031140744_0.addMenuItem("期刊中心","#");
+  mm_menu_1031140744_0.addMenuItem("研究机构","http://www.swufe.edu.cn/2191.html");
+  mm_menu_1031140744_0.addMenuItem("出版社","http://bookcj.com");
+mm_menu_1031140744_0.addMenuItem("西财指数","http://stat.swufe.edu.cn/zhishu");
+   mm_menu_1031140744_0.hideOnMouseOut=true;
+   mm_menu_1031140744_0.bgColor='#003a66';
+   mm_menu_1031140744_0.menuBorder=0;
+   mm_menu_1031140744_0.menuLiteBgColor='';
+   mm_menu_1031140744_0.menuBorderBgColor='';
+
+  window.mm_menu_1031140818_0 = new Menu("root",80,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,true);
+  mm_menu_1031140818_0.addMenuItem("本科生招生","http://zb.swufe.edu.cn");
+   mm_menu_1031140818_0.addMenuItem("研究生招生","http://yz.swufe.edu.cn/");
+mm_menu_1031140818_0.addMenuItem("专业学位教育招生","http://www.swufe.edu.cn/2183.html");
+    mm_menu_1031140818_0.addMenuItem("继续教育招生","http://xczx.swufe.edu.cn/");
+
+  mm_menu_1031140818_0.addMenuItem("留学生招生","http://international.swufe.edu.cn");
+mm_menu_1031140818_0.addMenuItem("出国预备教育","http://liuxue.swufe.edu.cn");
+
+   mm_menu_1031140818_0.addMenuItem("就业信息网","http://job.swufe.edu.cn");
+   mm_menu_1031140818_0.hideOnMouseOut=true;
+   mm_menu_1031140818_0.bgColor='#003a66';
+   mm_menu_1031140818_0.menuBorder=0;
+   mm_menu_1031140818_0.menuLiteBgColor='';
+   mm_menu_1031140818_0.menuBorderBgColor='';
+   
+     window.mm_menu_1031140818_0_dx = new Menu("root",20,28,"",12,"#003a66","#003a66","","","left","middle",2,0,100,-5,7,true,1,false,0,false,true);
+
+
+   mm_menu_1031140818_0_dx.addMenuItem("西财指数","http://stat.swufe.edu.cn/zhishu/index.asp");
+   mm_menu_1031140818_0_dx.hideOnMouseOut=true;
+   mm_menu_1031140818_0_dx.bgColor='#336699';
+   mm_menu_1031140818_0_dx.menuBorder=0;
+   mm_menu_1031140818_0_dx.menuLiteBgColor='';
+   mm_menu_1031140818_0_dx.menuBorderBgColor='';
+
+  window.mm_menu_1031140853_0 = new Menu("root",70,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,true);
+
+  mm_menu_1031140853_0.addMenuItem("国际合作","http://oiec.swufe.edu.cn");
+  mm_menu_1031140853_0.addMenuItem("国内合作","http://fzghc.swufe.edu.cn");
+  mm_menu_1031140853_0.addMenuItem("校友总会","http://xyzh.swufe.edu.cn");
+   mm_menu_1031140853_0.hideOnMouseOut=true;
+   mm_menu_1031140853_0.bgColor='#003a66';
+   mm_menu_1031140853_0.menuBorder=0;
+   mm_menu_1031140853_0.menuLiteBgColor='';
+   mm_menu_1031140853_0.menuBorderBgColor='';
+
+    window.mm_menu_1031140924_0 = new Menu("root",100,28,"",12,"#ffffff","#ffffff","","","left","middle",2,0,1000,-5,7,true,1,false,0,false,false);
+
+ /*
+ mm_menu_1031140924_0.addMenuItem("图书馆","http://www.lib.swufe.edu.cn");
+ mm_menu_1031140924_0.addMenuItem("后勤服务","http://houqin.swufe.edu.cn");
+ mm_menu_1031140924_0.addMenuItem("网络服务","http://info.swufe.edu.cn");
+ mm_menu_1031140924_0.addMenuItem("教育技术","http://info.swufe.edu.cn");
+ mm_menu_1031140924_0.addMenuItem("综合信息","<!--{#$ECMS_8711280238331149295864_LINKPAGEMORE#}-->");
+ mm_menu_1031140924_0.addMenuItem("网上收发室","http://houqin.swufe.edu.cn/post/baglist.asp");
+ */
+  mm_menu_1031140924_0.addMenuItem("在校学生","4213.html");
+ mm_menu_1031140924_0.addMenuItem("教职工","4214.html");
+ mm_menu_1031140924_0.addMenuItem("校友","4215.html");
+ mm_menu_1031140924_0.addMenuItem("考生及访客","4216.html");
+    mm_menu_1031140924_0.hideOnMouseOut=true;
+   mm_menu_1031140924_0.bgColor='#003a66';
+   mm_menu_1031140924_0.menuBorder=0;
+   mm_menu_1031140924_0.menuLiteBgColor='';
+   mm_menu_1031140924_0.menuBorderBgColor='';
+
+
+mm_menu_1031140924_0.writeMenus();
+}
